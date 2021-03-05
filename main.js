630b4136c68c809aca28fdee7bd07ad861164ff0
Ankit Gupta <gankit389@gmail.com>
2021-03-05 02:05:43 -0500

    second cipher in progress

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function displayAlgo(){
     document.getElementById(selected).style.display = "flex";
 }
 
-function eyncript(){
+function encrypt(){
     var list = document.getElementById("algo-select");
     var input = document.getElementById("input-string").value;
     var output;
@@ -26,7 +26,7 @@ function eyncript(){
             output = ceaserCihper(input);
             break;
         case 2:
-            albertiCihper();
+            output = vigenereCipher(input);
             break;
     }
     document.getElementById("output-string").innerHTML = output;
@@ -35,20 +35,48 @@ function ceaserCihper(text){
     var letters = document.getElementById("ceaser-input-alphabet").value;
     var shift = parseInt(document.getElementById("ceaser-input-shift").value);
     var message = text.toLowerCase(); 
-    var len = message.length;
+    var index;
 
     message = message.split("")
     letters = letters.split("");
 
-    for (var i = 0; i < len; i++){
+    for (var i = 0; i < message.length; i++){
         if (message[i] === " "){
             continue;
         }
-        var index = letters.indexOf(message[i]);
-        index = (index + shift) %26;
+        index = letters.indexOf(message[i]);
+        if(index === -1){
+            return "Error! Message has letters not in the alphabet"
+        }        
+        index = (index + shift) % (letters.length);
         message[i] = letters[index] 
     }
 
     return message.join("");
 
+}
+function vigenereCipher(text){
+    var key = document.getElementById("vigenere-input-key").value;
+    var letters = document.getElementById("vigenere-input-alphabet").value;
+    var message = text.toLowerCase();
+    var index; 
+
+    key = key.split();
+    message = message.split();
+    letters = letters.split();
+
+    for(var i= 0; i < message.length; i++){
+        if(message === ' '){
+            continue;
+        }
+        index = letters.indexOf(message[i]);
+
+        
+
+        index = (index + letters.indexOf(key[i%key.length]) % (letters.length));
+        message[i] = letters[index];
+        console.log(message);
+
+    }
+    return message.join("");
 }
